8e2532b593d9c9c1bee9a039ea324105d63b5abb
DOLADAOUD <doladaoud710@gmail.comIbrahim-99git config  --global  user.email doladaoud710@gmail.com>
2018-12-01 14:55:16 -0500

    about Project-1

---
+++ src/Welcome/Hello.java
@@ -0,0 +1,11 @@
+package Welcome;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Everyone! My name is Dola Daoud and I live in NYC");
+		
+
+	}
+
+}
